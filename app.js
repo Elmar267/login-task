241f3e71a8ff2ce1519ee89cf8481d673d1ae8e2
Elmar Ismayilzade <elmarismayilzad07@gmail.com>
2026-04-22 01:11:52 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const users = [
+    { email: "user@mail.com", pass: "8denebir" },
+    { email: "elmar@mail.com", pass: "01091939" },
+    { email: "artist@mail.com", pass: "02091945" }
+]
+const succes = document.getElementById('succes')
+const errorone = document.getElementById('errorone')
+const errortwo = document.getElementById('errortwo')
+
+function signIn() {
+
+    const inputemail = document.getElementById('inputemail').value
+    const inputPassword = document.getElementById('inputPassword').value
+
+    succes.style.display = "none"
+    errorone.style.display = "none"
+    errortwo.style.display = "none"
+    
+    if(users.find(item => item.email === inputemail && item.pass === inputPassword)){
+        succes.style.display = 'flex'
+    }else if(!inputemail.includes("@")){
+        errortwo.style.display = 'flex'
+    }else if(inputPassword.length < 6){
+        errortwo.style.display = 'flex'
+    }else{
+        errorone.style.display = 'flex'
+    }
+}
